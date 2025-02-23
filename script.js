92f0f52882ca328f82e73307099a8b9f2525339e
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-02-24 01:18:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-//your JS code here. If required.
+let input = document.querySelector("input");
+let button = document.querySelector("button");
+let output = document.querySelector("#output");
+
+button.addEventListener("click",(e)=>{
+	e.preventDefault();
+	new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(input.value);
+			
+		},2000);
+	})
+	.then(value=>{
+		return new Promise((resolve,reject)=>{
+			setTimeout(()=>{
+				output.innerText = `Result: ${value}`;
+				resolve(value*2);
+			},1000)
+		})
+	})
+	.then(value=>{
+		return new Promise((resolve,reject)=>{
+			setTimeout(()=>{
+				output.innerText = `Result: ${value}`;
+				resolve(value-3);
+			},1000)
+		})
+	})
+	.then(value=>{
+		return new Promise((resolve,reject)=>{
+			setTimeout(()=>{
+				output.innerText = `Result: ${value}`;
+				resolve(value/3);
+			},1000)
+		})
+	})
+	.then(value=>{
+		return new Promise((resolve,reject)=>{
+			setTimeout(()=>{
+				output.innerText = `Result: ${value}`;
+				resolve(value+10);
+			},1000)
+		})
+	})
+	.then(value=>{
+		output.innerText = `Result: ${value}`;
+	})
+})
+
+
+
